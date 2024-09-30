bcf4e931f1266a8ecb121a90e29ff373e65ee30c
Simrath Randhawa <srandhawa1250@gmail.com>
2024-09-30 09:12:54 -0500

    added the different components we will need

---
+++ components/navbar/NavBar.jsx
@@ -0,0 +1,18 @@
+
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/view-budget">View A Budget</Link></li>
+        <li><Link to="/view-all-budgets">View All Budgets</Link></li>
+        <li><Link to="/add-budget">Add New Budget</Link></li>
+        <li><Link to="/enter-money-spent">Add Money Spent To A Budget</Link></li>
+        <li><Link to="/login">Log Out</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
